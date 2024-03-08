6b4c31122a216520a09c76f1aecb79d2931a6b68
GerardoDurBa <gduranb@ufpso.edu.co>
2024-03-08 11:56:54 -0500

    Upgrading code

--- clase3/src/main/java/books/Book.java
+++ clase3/src/main/java/books/Book.java
@@ -2,23 +2,91 @@ package books;
 
 public class Book {
     String title;
-    int ammount;
     String genre;
+    String author;
+    int ammount;
     int id;
     String loanDate;
+    String user; 
     String returnDate;
     boolean available;
+    boolean lend;
 
-    public Book(String title, int ammount, String genre, int id) {
+    public Book(String title, String genre, String author, int ammount, int id, String loanDate, String user, String returnDate) {
         this.title = title;
-        this.ammount = ammount;
         this.genre = genre;
+        this.author = author;
+        this.ammount = ammount;
         this.id = id;
-        this.available = true;
+        this.loanDate = loanDate;
+        this.user = user;
+        this.returnDate = returnDate;
     }
-    
-    
-    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAmmount() {
+        return ammount;
+    }
+
+    public void setAmmount(int ammount) {
+        this.ammount = ammount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(String loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+       
     public void lendBook(){
         this.available=false;
     }
--- clase3/src/main/java/com/mycompany/clase3/Clase3.java
+++ clase3/src/main/java/com/mycompany/clase3/Clase3.java
@@ -5,13 +5,22 @@ import books.Book;
 public class Clase3 {
 
     public static void main(String[] args) {
-        Book book1 = new Book("Harry Potter", 5,"Fantasía", 25);
-        Book book2 = new Book("El Hombre Del Viento",3, "Aventura", 12);
+        Book book1 = new Book("Harry Potter", "Fantasía","¿?", 2, 78,"16/12/23","Fruntusio Federal","16/05/24");
+        Book book2 = new Book("Sangre de campeòn", "Acciòn","¿?", 1, 93,"17/02/24","Federico Fernandez ","17/07/24");
         
         //Presto el libro
+        System.out.println("Libro 1:");
         book1.lendBook();
-        
         System.out.println(book1.isAvailable());
+        System.out.println(book1.getTitle());
+        System.out.println(book1.getGenre());
+        System.out.println(book1.getAuthor());
+        System.out.println(book1.getAmmount());
+        System.out.println(book1.getId());
+        System.out.println(book1.getLoanDate());
+        System.out.println(book1.getUser());
+        System.out.println(book1.getReturnDate());
+        System.out.println("Libro 2:");
         System.out.println(book2.isAvailable());
         
     }

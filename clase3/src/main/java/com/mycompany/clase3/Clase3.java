f434eb660e8d9c2580534e8ed93aabdc16ec830d
GerardoDurBa <gduranb@ufpso.edu.co>
2024-03-11 11:51:17 -0500

    trabajando accesos

--- clase3/src/main/java/com/mycompany/clase3/Clase3.java
+++ clase3/src/main/java/com/mycompany/clase3/Clase3.java
@@ -6,7 +6,7 @@ public class Clase3 {
 
     public static void main(String[] args) {
         Book book1 = new Book("Harry Potter", "Fantasía","¿?", 2, 78,"16/12/23","Fruntusio Federal","16/05/24");
-        Book book2 = new Book("Sangre de campeòn", "Acciòn","¿?", 1, 93,"17/02/24","Federico Fernandez ","17/07/24");
+        Book book2 = new Book("Sangre de campeòn", "Acción","¿?", 1, 93,"17/02/24","Federico Fernandez ","17/07/24");
         
         //Presto el libro
         System.out.println("Libro 1:");
@@ -22,6 +22,15 @@ public class Clase3 {
         System.out.println(book1.getReturnDate());
         System.out.println("Libro 2:");
         System.out.println(book2.isAvailable());
+        System.out.println(book2.isAvailable());
+        System.out.println(book2.getTitle());
+        System.out.println(book2.getGenre());
+        System.out.println(book2.getAuthor());
+        System.out.println(book2.getAmmount());
+        System.out.println(book2.getId());
+        System.out.println(book2.getLoanDate());
+        System.out.println(book2.getUser());
+        System.out.println(book2.getReturnDate());
         
     }
 }
